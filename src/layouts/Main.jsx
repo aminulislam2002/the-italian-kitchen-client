4aa843e65cae02faf3d2c749ed15e5627b9241b2
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-01 17:24:00 -0700

    footer page add

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../pages/Shared/Header/Header";
-import Footer from "../pages/Shared/Footer/Footer";
+import Footer from "../pages/Home/Footer/Footer";
 import { Outlet } from "react-router-dom";
 
 const Main = () => {
---
+++ src/pages/Home/Footer/Footer.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="footer mt-5 pt-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4">
+              <h1>The Italian Kitchn</h1>
+              <p>
+                There are many variations of passages of Lorem Ipsum, but the majority have suffered alteration in some form.
+              </p>
+            </div>
+            <div class="col-xs-6 col-sm-3 col-md-2">
+              <h4>Company</h4>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Work</a>
+                </li>
+                <li>
+                  <a href="#">Latest News</a>
+                </li>
+                <li>
+                  <a href="#">Careers</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-xs-6 col-sm-3 col-md-2">
+              <h4>Product</h4>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">Prototype</a>
+                </li>
+                <li>
+                  <a href="#">Plans & Pricing</a>
+                </li>
+                <li>
+                  <a href="#">Customers</a>
+                </li>
+                <li>
+                  <a href="#">Integrations</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-xs-6 col-sm-3 col-md-2">
+              <h4>Support</h4>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">Help Desk</a>
+                </li>
+                <li>
+                  <a href="#">Sales</a>
+                </li>
+                <li>
+                  <a href="#">Become a Partner</a>
+                </li>
+                <li>
+                  <a href="#">Developers</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
+              <h4>Contact</h4>
+              <p>+1 777 - 978 - 5570</p>
+              <p>524 Broadway , NYC</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
