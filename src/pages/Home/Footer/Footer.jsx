fe4190a5f0044c28af2dc991a80c474c059ed8f6
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-01 17:38:54 -0700

    login and register page add

--- src/pages/Home/Footer/Footer.jsx
+++ src/pages/Home/Footer/Footer.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer class="footer mt-5 pt-5">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-4">
+      <footer className="footer mt-5 pt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-sm-6 col-md-4">
               <h1>The Italian Kitchn</h1>
               <p>
                 There are many variations of passages of Lorem Ipsum, but the majority have suffered alteration in some form.
               </p>
             </div>
-            <div class="col-xs-6 col-sm-3 col-md-2">
+            <div className="col-xs-6 col-sm-3 col-md-2">
               <h4>Company</h4>
-              <ul class="list-unstyled">
+              <ul className="list-unstyled">
                 <li>
                   <a href="#">About Us</a>
                 </li>
@@ -29,9 +29,9 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div class="col-xs-6 col-sm-3 col-md-2">
+            <div className="col-xs-6 col-sm-3 col-md-2">
               <h4>Product</h4>
-              <ul class="list-unstyled">
+              <ul className="list-unstyled">
                 <li>
                   <a href="#">Prototype</a>
                 </li>
@@ -46,9 +46,9 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div class="col-xs-6 col-sm-3 col-md-2">
+            <div className="col-xs-6 col-sm-3 col-md-2">
               <h4>Support</h4>
-              <ul class="list-unstyled">
+              <ul className="list-unstyled">
                 <li>
                   <a href="#">Help Desk</a>
                 </li>
@@ -63,7 +63,7 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
+            <div className="col-xs-6 col-sm-3 col-md-3 col-lg-2">
               <h4>Contact</h4>
               <p>+1 777 - 978 - 5570</p>
               <p>524 Broadway , NYC</p>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
       },
     ],
   },
