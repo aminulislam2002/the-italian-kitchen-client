be318a12b60ed9b8c0c88053a7290a0f2077cb66
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-03 15:55:31 -0700

    hide environment variables

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC5XxUiC3v3VchIQN224ny-6-Eu8c7oY18",
-  authDomain: "assignment-number-ten.firebaseapp.com",
-  projectId: "assignment-number-ten",
-  storageBucket: "assignment-number-ten.appspot.com",
-  messagingSenderId: "889399094100",
-  appId: "1:889399094100:web:3596a03123b1e53da6b6c1",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
